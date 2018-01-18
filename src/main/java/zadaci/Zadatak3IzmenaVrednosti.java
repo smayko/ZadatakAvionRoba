88db262c99e89958125a96852975110a01404d12
Nikola Kosmajac <kosmajac@gmail.com>
2018-01-18 19:34:43 +0100

    Uradjen zadatak izmena vrednosti close #8

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,15 +1,36 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Roba;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Zadatak3IzmenaVrednosti {
     public static void main(String[] args) {
         try {
             ConnectionSource connectionSource = new JdbcConnectionSource(Konstante.DATABASE);
+
+            Dao<Roba, Integer> robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+          List<Roba> roba = robaDao.queryForAll();
+          for(Roba r : roba){
+              System.out.println("ID: " + r.getId() + "Opis: " + r.getOpis()+ " naziv: " +r.getNaziv());
+              if(r.getOpis().equals("plasticna stolica"));
+              r.setOpis("Drvena Stolica");
+              robaDao.update(r);
+          }
+
+
+            List<Roba> robaizmenjena = robaDao.queryForAll();
+            for(Roba r : robaizmenjena){
+                System.out.println("ID: " + r.getId() + "Opis: " + r.getOpis()+ " naziv: " +r.getNaziv());
+
+            }
 
 
             try {
