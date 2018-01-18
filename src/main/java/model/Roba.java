1b093a12bfaa55c34171a9c31e9bb1010a9efbce
Nikola Kosmajac <kosmajac@gmail.com>
2018-01-18 19:00:37 +0100

    Uradjena klasa roba close #5

--- src/main/java/model/Roba.java
+++ src/main/java/model/Roba.java
@@ -1,5 +1,78 @@
 package model;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "roba")
 public class Roba {
 
+    @DatabaseField(canBeNull = false, generatedId = true)
+   private int id;
+    @DatabaseField (columnName = POLJE_NAZIV, canBeNull = false)
+   private String naziv;
+    @DatabaseField(columnName = POLJE_OPIS, canBeNull = false)
+   private String opis;
+    @DatabaseField(columnName = POLJE_TEZINA, canBeNull = false)
+   private double tezina;
+    @DatabaseField
+    private Avion avion;
+
+   static final String POLJE_NAZIV = "";
+   static final String POLJE_OPIS = "";
+   static final String POLJE_TEZINA = "";
+
+    public Roba() {
+    }
+
+    public Roba(String naziv, String opis, double tezina) {
+        this.naziv = naziv;
+        this.opis = opis;
+        this.tezina = tezina;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("ID: " + id + "Naziv:"+naziv+ "Opis " +opis+"Tezina: " + tezina);
+        return super.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public double getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(double tezina) {
+        this.tezina = tezina;
+    }
+
+    public Avion getAvion() {
+        return avion;
+    }
+
+    public void setAvion(Avion avion) {
+        this.avion = avion;
+    }
 }
