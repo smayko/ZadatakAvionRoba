a9c1851236adfd373211504cef1677afabc9483b
Nikola Kosmajac <kosmajac@gmail.com>
2018-01-18 18:51:19 +0100

    Uradjena klasa avion close #4

--- src/main/java/model/Avion.java
+++ src/main/java/model/Avion.java
@@ -1,4 +1,69 @@
 package model;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.ArrayList;
+
+@DatabaseTable(tableName = "avion")
 public class Avion {
+
+    @DatabaseField(columnName = "id", canBeNull = false, generatedId = true)
+   private int id;
+    @DatabaseField(columnName = POLJE_OZNAKA, canBeNull = false)
+   private String oznaka;
+    @DatabaseField(columnName = POLJE_RASPONKRILA, canBeNull = false)
+   private int rasponKrila;
+    @ForeignCollectionField
+    ArrayList<Roba> roba;
+
+   static final String POLJE_OZNAKA ="oznaka";
+   static final String POLJE_RASPONKRILA ="raspon krila";
+
+    public Avion() {
+    }
+
+    public Avion(String oznaka, int rasponKrila) {
+        this.oznaka = oznaka;
+        this.rasponKrila = rasponKrila;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("ID: " + id + "Oznaka: " + oznaka + "Raspon Krila: " + rasponKrila);
+        return super.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+
+    public int getRasponKrila() {
+        return rasponKrila;
+    }
+
+    public void setRasponKrila(int rasponKrila) {
+        this.rasponKrila = rasponKrila;
+    }
+
+    public ArrayList<Roba> getRoba() {
+        return roba;
+    }
+
+    public void setRoba(ArrayList<Roba> roba) {
+        this.roba = roba;
+    }
 }
