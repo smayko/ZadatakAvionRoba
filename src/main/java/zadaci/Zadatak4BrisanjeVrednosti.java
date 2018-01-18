08e82300fa674927f2f80756c738868473e02012
Nikola Kosmajac <kosmajac@gmail.com>
2018-01-18 19:39:54 +0100

    Uradjen zadatak brisanja vrednosti close #9

--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,10 +1,14 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Roba;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Zadatak4BrisanjeVrednosti {
     public static void main(String[] args) {
@@ -12,6 +16,20 @@ public class Zadatak4BrisanjeVrednosti {
         try {
             ConnectionSource connectionSource = new JdbcConnectionSource(Konstante.DATABASE);
 
+
+            Dao<Roba, Integer> robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+            List<Roba> roba = robaDao.queryForAll();
+            for(Roba r : roba){
+                if(r.getNaziv().equals("voda")){
+                    robaDao.delete(r);
+                }
+            }
+
+            List<Roba> robaIzmenjena = robaDao.queryForAll();
+            for(Roba r : robaIzmenjena){
+                System.out.println("ID: " + r.getId() +"naziv" + r.getNaziv()+"opsi: "+ r.getOpis());
+            }
 
             try {
                 connectionSource.close();
